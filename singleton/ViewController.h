ff48f33f72aaf941dc77a060399529a7cbca2672
wcj <wcj@wcjdemac-mini.local>
2016-03-16 14:23:33 +0800

    Initial Commit

---
+++ singleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  singleton
+//
+//  Created by wcj on 16/3/16.
+//  Copyright © 2016年 bodhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
